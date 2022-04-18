6a0e3af8d1ac345975801722029a7e04259c4635
prajwal-h-v <prajji14@gmail.com>
2022-04-18 12:30:22 +0530

    Update UI

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -1,8 +1,8 @@
-var ready = (callback) => {
-  if (document.readyState != "loading") callback();
-  else document.addEventListener("DOMContentLoaded", callback);
-};
+// var ready = (callback) => {
+//   if (document.readyState != "loading") callback();
+//   else document.addEventListener("DOMContentLoaded", callback);
+// };
 
-ready(() => {
-  document.querySelector(".header").style.height = window.innerHeight + "px";
-});
+// ready(() => {
+//   document.querySelector(".header").style.height = window.innerHeight + "px";
+// });
