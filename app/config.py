76f2931ee53a50f46746cd28d9d1d84b29f62d1b
prajwal-h-v <prajji14@gmail.com>
2022-04-22 09:59:49 +0530

    Gitignore conifg

--- app/config.py
+++ app/config.py
@@ -1,2 +1,4 @@
 weather_api_key = "1a8e7a45cdec4a42c22da73ef47d9b4d"
 MONGODB_URI =    'mongodb+srv://prajji14:prajjivk169@cluster0.p8upr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+
